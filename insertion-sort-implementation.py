8c0822e9d4aa4e6bfeb6256c73f491442f6f81f7
addisumotora <addisumotora3@gmail.com>
2023-10-22 14:39:01 +0300

    insertion-sort

---
+++ insertion-sort-implementation.py
@@ -0,0 +1,17 @@
+def insertion_sort(array):
+    for i in range(1, len(array)):
+
+        current_value = array[i];
+
+        j = i - 1
+
+        while j >=0 and current_value < array[j]:
+            array[j + 1] = array[j]
+            j-=1
+
+        array[j + 1] = current_value 
+
+
+    return array
+
+print(insertion_sort([12, 11, 13, 5, 6]))
