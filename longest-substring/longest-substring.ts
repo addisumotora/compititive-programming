e7f8359f60874b9c511b3db2b3bd5e6eb3f63d4d
addisumotora <addisumotora3@gmail.com>
2023-12-24 15:31:09 +0300

    longest-substring-ts

--- longest-substring/longest-substring.ts
+++ longest-substring/longest-substring.ts
@@ -1,37 +1,32 @@
-/**
- * @param {string} s
- * @return {string}
- */
-var longestPalindrome = function(s) {
-    let res = '';
-    let resLength = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        let l = i;
-        let r = i;
-
-        while (l >= 0 && r < s.length && s[l] === s[r]) {
-            if ((r - l + 1) > resLength) {
-                res = s.slice(l, r + 1);
-                resLength = r - l + 1;
-            }
-
-            l--;
-            r++;
-        }
-
-        l = i;
-        r = i + 1;
-
-        while (l >= 0 && r < s.length && s[l] === s[r]) {
-            if ((r - l + 1) > resLength) {
-                res = s.slice(l, r + 1);
-                resLength = r - l + 1;
-            }
-            l--;
-            r++;
-        }
-    }
-
-    return res;
-};
+function longestPalindromets(s: string): string {
+    let res = ''
+    let resLength = 0
+ 
+     for (let i = 0; i < s.length; i++) {
+         let l = i;
+         let r = i;
+ 
+         while (l >= 0 && r < s.length && s[l] === s[r]) {
+             if (r - l + 1 > resLength) {
+                 res = s.slice(l, r + 1);
+                 resLength = r - l + 1;
+             }
+ 
+             l--;
+             r++;
+         }
+ 
+         l = i;
+         r = i + 1;
+ 
+         while (l >= 0 && r < s.length && s[l] === s[r]) {
+             if (r - l + 1 > resLength) {
+                 res = s.slice(l, r + 1);
+                 resLength = r - l + 1;
+             }
+             l--;
+             r++;
+         }
+     }
+  return res;
+ };
